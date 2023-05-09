e708b7fd9e111cb7c4f8f78b2ae173bdf6856121
James Demlow <jdemlow@gmail.com>
2023-05-09 00:32:24 -0600

    about to config firebase

--- src/compontents/Todo.jsx
+++ src/compontents/Todo.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
-const todo = () => {
-  return <li>Todo</li>;
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer items-center`,
+};
+
+const todo = ({ todo }) => {
+  return (
+    <li className={style.li}>
+      <div className={style.row}>
+        <input type="checkbox" />
+        <p className={style.text}>{todo}</p>
+      </div>
+      <button>{<FaRegTrashAlt />}</button>
+    </li>
+  );
 };
 
 export default todo;
--- src/pages/TodoApp.jsx
+++ src/pages/TodoApp.jsx
@@ -5,9 +5,15 @@ import Todo from "../compontents/Todo";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1C85E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl fonnt-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 };
 
-const TodoApp = () => {
+function TodoApp() {
   const [todos, setTodos] = useState(["Learn React", "Grind Leetcode"]);
 
   return (
@@ -21,11 +27,14 @@ const TodoApp = () => {
           </button>
         </form>
         <ul>
-          <Todo />
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
         </ul>
+        <p className={style.count}>You have 2 todos</p>
       </div>
     </div>
   );
-};
+}
 
 export default TodoApp;
