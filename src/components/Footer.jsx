f94e4a388d75553a7fe49f788251617572e52014
JDemlow <jdemlowdev@gmail.com>
2024-06-25 19:19:57 -0600

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Footer = () => {
+  const date = () => {
+    const d = new Date();
+    return d;
+  };
   return (
     <div>
       <footer className="bg-gray-800 text-center dark:bg-neutral-700 lg:text-left">
         <div className="p-4 text-center text-white dark:text-neutral-200">
-          © 2023 Copyright: Pure Hospitality
+          Copyright {date().getFullYear()}: Pure Hospitality
         </div>
       </footer>
     </div>
