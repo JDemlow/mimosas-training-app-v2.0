b52ec88cf8fa9ef37a205e2ae414125539d7de46
JDemlow <jdemlowdev@gmail.com>
2024-06-28 01:58:50 -0600

    calendar

--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -5,6 +5,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { db } from "../firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { Modal, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"; // Ensure this is imported
 
 // Setup the localizer by providing the moment (or globalize) Object
 const localizer = momentLocalizer(moment);
@@ -78,7 +79,7 @@ const MyCalendar = () => {
             <div className="mb-6 md:flex md:items-center">
               <div className="md:w-1/3">
                 <label
-                  className="mb-1 block pr-4 font-bold text-gray-500 md:mb-0 md:text-right"
+                  className="block pr-4 mb-1 font-bold text-gray-500 md:mb-0 md:text-right"
                   htmlFor="title"
                 >
                   Event Title
